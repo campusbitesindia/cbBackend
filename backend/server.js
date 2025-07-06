f83e904c375ac5196ccbb42f64b34807d25789ac
jangradev27 <jangradev14@gmail.com>
2025-07-06 10:52:30 +0530

    Order controllers created

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const {connectDB} = require("./config/database");
-
+const AuthRoutes=require("./routes/userRoutes")
+const OrderRoutes =require("./routes/OrderRoutes");
 dotenv.config({path: "./config/config.env"});
-connectDB();
+
+
+app.use("/auth",AuthRoutes)
+app.use("/order",OrderRoutes)
+ 
 app.listen(process.env.port, ()=>{
     console.log(`Server is up on port: ${process.env.port}`);
 })
+connectDB();
