d8548b4b1e3e2783b83f599bf42e38f1f6251b33
jangradev27 <jangradev14@gmail.com>
2025-07-11 12:24:34 +0530

    conflicts resolved

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 const {connectDB} = require("./config/database");
-const app=require("../backend/app")
+
 dotenv.config({path: "./config/config.env"});
 
 
