ff9524510bd0a513e30858364dfb63b1aa28d5f1
Venkatasantosh <iam894083@gmail.com>
2025-07-11 10:41:49 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const { getMyOrders, createOrder, getOrderById } = require('../controllers/orderController');
-const { protect } = require('../middleware/auth');
 
-// All routes here are protected
-router.route('/')
-    .get(protect, getMyOrders)
-    .post(protect, createOrder);
-
-router.route('/:id')
-    .get(protect, getOrderById);
-
-module.exports = router; 
