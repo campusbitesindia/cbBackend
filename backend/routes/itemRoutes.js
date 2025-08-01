582d05bb9e4ad2dc5e6c4dcfbc1bf211c70f2079
jangradev27 <jangradev14@gmail.com>
2025-08-01 22:08:54 +0530

    quickBites updated

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getItems, createItem, updateItem, deleteItem, getItemsUnder99, getItemsByPriceRange, getReadyItems, toggleItemReadyStatus } = require('../controllers/itemController')
+const { getItems, createItem, updateItem, deleteItem, getItemsUnder99, getItemsByPriceRange, getReadyItems, toggleItemReadyStatus, getReadyItemsofAllCanteens } = require('../controllers/itemController')
 const{ isAuthenticated, isVendor }=require("../middleware/auth");
 const upload = require('../middleware/uploadMiddleware');
 
@@ -12,6 +12,6 @@ router.route("/under99/:id").get(getItemsUnder99);
 router.route("/range/:id").get(getItemsByPriceRange);
 router.route("/ready/:id").get(getReadyItems);
 router.route("/toggle-ready/:id").put(toggleItemReadyStatus);
-
-module.exports = router;
+router.get("/allReadyItems",getReadyItemsofAllCanteens);
+module.exports = router; 
  
