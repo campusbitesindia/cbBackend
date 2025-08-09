a71871e16c1ceaca507981bf73151b27bfcb716a
jangradev27 <jangradev14@gmail.com>
2025-08-09 18:35:20 +0530

    Phone Number problem resolved

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -22,7 +22,6 @@ router.route("/profile/image").post(isAuthenticated, upload.single('profileImage
 
 // // Google OAuth
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
 router.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: 'https://campus-bites-c7pe.vercel.app/login', session: false }),
   (req, res) => { 
